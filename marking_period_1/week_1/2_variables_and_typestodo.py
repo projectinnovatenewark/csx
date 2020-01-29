371529f79f31a393cecf17162a88528735cfb279
Francine <francine.t.karpeh@gmail.com>
2020-01-28 21:10:11 -0500

    revise the first todo

--- marking_period_1/week_1/2_variables_and_typestodo.py
+++ marking_period_1/week_1/2_variables_and_typestodo.py
@@ -4,6 +4,19 @@ Task- variables and types
 
 # TODO: make a string and concatenate it with numbers, then print the result
 
+# TODO: make your name into a string, then make your age into an integer. Concatenate using
+# TODO: "+" and print the result. Then use "," to concatenate. The output
+# TODO: should be "My name is blank and I am blank years old". 
+
 # TODO: set a float equal to a variable, then print that float
 
+# TODO: set a variable equal to the float `1.`, then set another variable equal
+# TODO: to your age. Concatenate and print the result. 
+
+
 # TODO: use the function `type([variable-here])` to print examples of the types we have worked
+
+# TODO: set each value type we learned equal to variables, then use the function `type([variable_here])` 
+# TODO: to print examples of the types we have worked. Re-do and wrap the string function around each variable. 
+
+
