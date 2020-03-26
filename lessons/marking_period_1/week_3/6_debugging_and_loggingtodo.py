2e290307bf6b2b19b45a8bf07c02c90462a35bc6
Christian <chrisadrianotav@gmail.com>
2020-03-26 16:20:17 -0400

    finish oop lesson

--- lessons/marking_period_1/week_3/6_debugging_and_loggingtodo.py
+++ lessons/marking_period_1/week_3/6_debugging_and_loggingtodo.py
@@ -1,12 +1,10 @@
 """
-Match up with a partner. One of you should solve the first problem, the other should solve the second.
-You will then break your working code and trade the broken solutions. The other person will have to 
+Match up with a partner. One person should solve the 1st problem, the other should solve the 2nd.
+You will then break your working code and trade the broken solutions. The other person will have to
 fix your broken code.
 """
 
-
-
-# Problem 1:
+# TODO: Problem 1:
 # Write a function called multiplier. it should accept two arguments. The first arg, "l", should be
 # a list of numbers. The second arg, "m", should be a number. In the function, iterate through
 # the list of numbers and multiply them by the multiplier. First, print out the list that was
@@ -19,12 +17,12 @@ num_data_one = [15, 12, 22, 8, 4]
 multiplier_two = 4
 num_data_two = [5, 88, 64, 35]
 
-# Problem 2:
-# Write a function that takes in an argument. The argument should be the dictioary below, titled
-# uncurved grades. You should iterate through every score in the attendance and curve it
-# up by 10%. The homework should curve by 2%. The midterm one should be curved by 15%.
-# The midterm two should be curved by 20%. Return a new variable called curved_grades
-# with the newly curved values.
+# TODO: Problem 2:
+# Write a function that takes in an argument of "grades". The argument should be the
+# dictioary below, titled uncurved grades. You should iterate through every score in the
+# attendance and curve it up by 10%. The homework should curve by 2%. The midterm one should
+# be curved by 15%. The midterm two should be curved by 20%. Return a new variable called
+# curved_grades with the newly curved values.
 
 uncurved_grades = {
     "attendance": [80, 90, 86, 94, 100, 64, 73],
