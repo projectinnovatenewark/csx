6b53ea987b2e276235946d3d47a3ec366b6a987c
markskurat <markskurat136@gmail.com>
2020-07-27 18:08:13 -0400

    Changes to classes lesson

--- lessons/students/marking_period_1/week_3/16_classes.py
+++ lessons/students/marking_period_1/week_3/16_classes.py
@@ -18,23 +18,23 @@ Understanding classes and scope within a class
 ​
 # As shown below, we also pass an inital value into the object Cat(). We then set
 # the input to "name" inside the init constructor and set the Cat's name value
-# to the input it receives
+# to the input it receives.
 ​
 # For more info on classes, refer to the Python docs:
 # https://docs.python.org/3/tutorial/classes.html
 ​
 class Cat:
 ​
-    kind = 'feline'         # class attribute shared by all instances
+    kind = 'feline'         # Class attribute shared by all instances.
 ​
     def __init__(self, name):
-        self.name = name    # instance attribute unique to each instance
+        self.name = name    # Instance attribute unique to each instance.
 ​
-# this will create two new instances of a Cat class with the self.names of Fido and Bella
+# This will create two new instances of a Cat class with the self.names of Fido and Bella.
 d = Cat('Fido')
 e = Cat('Bella')
 ​
-# since kind is declared for all cats, the kind will be shared by both Fido and Bella
+# Since kind is declared for all cats, the kind will be shared by both Fido and Bella.
 print("d.kind", d.kind)                  # shared by all Cats
 print("e.kind", e.kind)                  # shared by all Cats
 print("d.name", d.name)                  # unique to d
@@ -44,7 +44,7 @@ print("e.name", e.name)                  # unique to e
 ​
 class Cat:
 ​
-    tricks = []  # mistaken use of a class attribute shared by all Cats due to scope
+    tricks = []  # Mistaken use of a class attribute shared by all Cats due to scope.
 ​
     def __init__(self, name):
         self.name = name
@@ -56,8 +56,8 @@ d = Cat('Fido')
 e = Cat('Bella')
 d.add_trick('hit yarn')
 e.add_trick('attack bird')
-print(d.tricks)  # unexpectedly shared by all dogs. that is because tricks was set as a class
-                 # attribute rather than an instance attribute. more on that in the link below:
+print(d.tricks)  # Unexpectedly shared by all dogs. That is because tricks was set as a class
+                 # attribute rather than an instance attribute. More on that in the link below:
                  # https://dzone.com/articles/python-class-attributes-vs-instance-attributes#:~:text=A%20class%20attribute%20is%20a,.)%20%2C%20of%20the%20class.
 ​
 ####################################################################################################
@@ -66,7 +66,7 @@ class Cat:
 ​
     def __init__(self, name):
         self.name = name
-        self.tricks = []  # creates a new empty list for each Cat
+        self.tricks = []  # creates a new empty list for each cat
 ​
     def add_trick(self, trick):
         self.tricks.append(trick)
@@ -85,18 +85,19 @@ class Classmate:
         self.city = city
         self.age = age
 ​
-# TODO: Section 1 of the TODO 3.4
+# TODO: Section 1 of the TODO 16
 
 ########################################################################################
 
-    # this is a function
+    # This is a function:
     def format_class(self):
-        # here we will create a list of items to print within the class definition
+        # Here we will create a list of items to print within the class definition
         # and then join them to create one string. The dashes are just for readability.
         s = ["----------",
-             # the f before each string allows for f-string literals
+             # The f before each string allows for f-string literals
              # these allow for the format() function to be used, and for
-             # curly braces to be used for replacement fields. link for reference:
+             # curly braces to be used for replacement fields. See the link for 
+             # reference:
              # https://docs.python.org/3/whatsnew/3.6.html#whatsnew36-pep498
              f"First Name : {self.first_name}",
              f"Last Name : {self.last_name}",
@@ -104,56 +105,61 @@ class Classmate:
              f"Age : {self.age}"]
         return print("\n".join(s))
 ​
-# TODO: Section 2 of the TODO 3.4
+# TODO: Section 2 of the TODO 16
 
 ####################################################################################################
 
 def complete_form(n_users):
-    """The function will ask questions with the helper function of ask_user() and create Classmates.
-    This function will return a list with the number of classmates you passed to this function."""
-    # here we used a list comprehension to create classes for the amount n_users
-    # and add them to the list
+    """
+    The function will ask questions with the helper function of ask_user() and create Classmates.
+    This function will return a list with the number of classmates you passed to this function.
+    """
+    
+    # Here we used a list comprehension to create classes for the amount n_users
+    # and add them to the list.
     value = [Classmate(ask_user("Enter First Name: ").title(),
                        ask_user("Enter Last Name: ").title(),
                        ask_user("Enter City: ").title(),
-                       # this type=int specifies that the age input will
-                       # only work if it is passed a number
+                       # This type=int specifies that the age input will
+                       # only work if it is passed a number.
                        ask_user("Enter Age: ", type=int)
                       )
              for question in range(n_users)
             ]
-    # this will print the 3 Classmate objects that we arranged in our list comprehension above
+    # This will print the 3 Classmate objects that we arranged in our list comprehension above.
     print(value)
     return value
 ​
 def ask_user(message='', type=str):
-    """this is a helper function for getting the class of Classmate's inputs"""
+    """This is a helper function for getting the class of Classmate's inputs"""
+
     user_input = ''
     while not user_input:
         try:
-            # we added a call to str.strip, so user names like (spaces) and (tabs) are not allowed.
+            # We added a call to str.strip, so user names like (spaces) and (tabs) are not allowed.
             # if spaces are entered, a ValueError will be thrown and the question will be repeated.
-            # since the default type is str (for string), it will convert the input to a string. But
-            # when we pass the type as int, it will convert the user input to an integer
+            # Since the default type is str (for string), it will convert the input to a string. But
+            # when we pass the type as int, it will convert the user input to an integer.
             user_input = type(input(message).strip())
         except ValueError:
-            # if this recieves a value error, it will reiterate the same question until the input is satisfied
+            # If this recieves a value error, it will reiterate the same question until the input is 
+            # satisfied.
             continue
     return user_input
 ​
-# this line of code is explained very well here:
+# This line of code is explained very well here:
 # https://stackoverflow.com/questions/419163/what-does-if-name-main-do
-# the short answer from that question is here:
+# The short answer from that question is here:
 # https://stackoverflow.com/a/419986/9662099
 ​
 if __name__ == '__main__':
 ​
-    # here we specify that we want three classes of Classmate to be instantiated
+    # Here we specify that we want three classes of Classmate to be instantiated.
     USERS = complete_form(3)
 ​
-    # since the complete_form function returns a list of Classmates, we can iterate
+    # Since the complete_form function returns a list of Classmates, we can iterate
     # through that list with a for loop. Then, we will call the format_class function for
-    # each of those objects
+    # each of those objects.
     for a in range(len(USERS)):
         USERS[a].format_class()
 
@@ -174,7 +180,7 @@ class Person:
     def identifier(self):
         print("My name is {} {}".format(self.fname, self.lname))
 
-# Let's say we want to create a child class for 'Students'
+# Let's say we want to create a child class for 'Students'.
 # That would look something like this:
 # By using the super() function, you do not have to use the name of the parent element, 
 # it will automatically inherit the methods and properties from its parent.
@@ -188,12 +194,12 @@ class Student(Person):
         print("This is {} {} and they graduate in {}".format(self.fname, self.lname, self.grad_year))
 
 s1 = Student("Tiny", "Tim", 2020)
-# You can still call functions that are defined in the parent class
+# You can still call functions that are defined in the parent class.
 s1.identifier()
-# And here you see that you can call the function from the derived child class
+# And here you see that you can call the function from the derived child class.
 s1.welcome()
 
-# TODO: Section 3 & 4 of the TODO 3.4
+# TODO: Section 3 & 4 of the TODO 16
 
 ###################################################################################################
 
