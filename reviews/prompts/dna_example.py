c77ca3701c1d3dd51ff01bc131b5e75998e82e3f
palmerigarrett <palmerigarrett@gmail.com>
2020-08-04 08:25:54 -0400

    added review problem from zoom lesson 8-4-20

---
+++ reviews/prompts/dna_example.py
@@ -0,0 +1,23 @@
+"""Reading a DNA sequence from a text file"""
+
+# TODO: A base pair is two chemical bases bonded to one another forming a
+# TODO: "rung of the DNA ladder." The DNA molecule consists of two strands
+# TODO: that wind around each other like a twisted ladder. Each strand has
+# TODO: a backbone made of alternating sugar (deoxyribose) and phosphate
+# TODO: groups. Attached to each sugar is one of four bases--adenine (A),
+# TODO: cytosine (C), guanine (G), or thymine (T). The two strands are held
+# TODO: together by hydrogen bonds between the bases, with adenine forming
+# TODO: a base pair with thymine, and cytosine forming a base pair with guanine.
+# TODO: In dna.txt, we have a 1000 character dna sequence. In this todo, you
+# TODO: will open the txt file, extract the data, and calculate the number
+# TODO: of occurrences for each of the bases- adenine, cytosine, guanine,
+# TODO: and thymine.
+
+# TODO: Now, write a new line for each of these bases and
+# TODO: their counts to the new file. Each new line in the file should read
+# TODO: "[base name here] occurs [number here] times in the DNA sequence".
+
+# TODO: Then, read the txt file line-by-line and print it to the terminal.
+
+# TODO: Close the file, which is standard to do once you are finished using
+# TODO: a .txt file.
