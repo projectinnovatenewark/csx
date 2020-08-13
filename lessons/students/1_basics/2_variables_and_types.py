d57f18fea7389c87641eaa6c7939427f34bf179c
Christian <chrisadrianotav@gmail.com>
2020-08-12 20:57:03 -0400

    spelling error

--- lessons/students/1_basics/2_variables_and_types.py
+++ lessons/students/1_basics/2_variables_and_types.py
@@ -1,63 +1,63 @@
-# """
-# This is a docstring. They are used to detail files at a high level.
-# """
+"""
+This is a docstring. They are used to detail files at a high level.
+"""
 
-# # These lines that start with a pound sign are comments. They do nothing to the program
-# # except provide instructions, personal notes, or anything that you want really!
+# These lines that start with a pound sign are comments. They do nothing to the program
+# except provide instructions, personal notes, or anything that you want really!
 
-# # TITLE  Section 1 (5 minutes)
+# TITLE  Section 1 (5 minutes)
 
-# # Variables can be thought of as "pointers" to a value. These values have types.
-# # Types include things like integers, strings, floats, booleans, lists, and dictionaries.
-# # The name of the variable is on the left of the "=" sign and on the right is the content
-# # of the variable.
+# Variables can be thought of as "pointers" to a value. These values have types.
+# Types include things like integers, strings, floats, booleans, lists, and dictionaries.
+# The name of the variable is on the left of the "=" sign and on the right is the content
+# of the variable.
 
-# # A number without a decimal point is of the type integer.
-# number1 = 5
+# A number without a decimal point is of the type integer.
+number1 = 5
 
-# # A number with a decimal point is a float.
-# float1 = 6.3 # Floating point number
-# float2 = 7. # This is also a floating point number.
+# A number with a decimal point is a float.
+float1 = 6.3 # Floating point number
+float2 = 7. # This is also a floating point number.
 
-# # Anything wrapped in quotation marks is a string.
-# string1 = "Hello" # String over here!
-# string2 = "my name is robit" # Literally, anything wrapped in quotes is a string.
-# string3 = "369" # Yes this is a string too.
+# Anything wrapped in quotation marks is a string.
+string1 = "Hello" # String over here!
+string2 = "my name is robot" # Literally, anything wrapped in quotes is a string.
+string3 = "369" # Yes this is a string too.
 
-# # Naming variables is important! This has a capital "S" in string, so it's different
-# # than the previous variable of 'string1'.
-# String1 = "Wonderful!"
+# Naming variables is important! This has a capital "S" in string, so it's different
+# than the previous variable of 'string1'.
+String1 = "Wonderful!"
 
-# # MULTIPLE VARIABLES IN ONE VARIABLE?! YEP!
-# big_string = string1 + string2
-# print(big_string)
+# MULTIPLE VARIABLES IN ONE VARIABLE?! YEP!
+big_string = string1 + string2
+print(big_string)
 
-# # When you see, TODO: Section x of TODO y, you should stop where you are and head over to
-# # the corresponding assignment for this section of the lesson. The line below instructs
-# # you to visit section 1 of the 2nd TODO and complete it before moving on to the next section.
-# # TODO: Section 1 of TODO 2 (2 minutes for students, 1 minute demo)
+# When you see, TODO: Section x of TODO y, you should stop where you are and head over to
+# the corresponding assignment for this section of the lesson. The line below instructs
+# you to visit section 1 of the 2nd TODO and complete it before moving on to the next section.
+# TODO: Section 1 of TODO 2 (2 minutes for students, 1 minute demo)
 
-# ####################################################################################################
+####################################################################################################
 
-# # TITLE  Section 2 (2 minutes)
+# TITLE  Section 2 (2 minutes)
 
-# # Setting multiple variables in one line of code? Yep. This is possible only when you use
-# # commas to separate each variable. The positions of the varibale names correspond with the values.
-# var1, var2, var3 = 1, 2, 3
-# print(var1, var2, var3)
+# Setting multiple variables in one line of code? Yep. This is possible only when you use
+# commas to separate each variable. The positions of the varibale names correspond with the values.
+var1, var2, var3 = 1, 2, 3
+print(var1, var2, var3)
 
-# # Booleans are true or false values. Python knows it is a boolean when you capitalize the first
-# # letter in your value just like below!
-# boolean = False
-# boolean2 = True
+# Booleans are true or false values. Python knows it is a boolean when you capitalize the first
+# letter in your value just like below!
+boolean = False
+boolean2 = True
 
-# # if you want to set a variable to an empty value, use the None type.
-# variable_for_later = None
-# print("variable for later will equal", variable_for_later)
+# if you want to set a variable to an empty value, use the None type.
+variable_for_later = None
+print("variable for later will equal", variable_for_later)
 
-# # TODO: Section 2 of TODO 2 (2 minutes for students, 1 minute demo)
+# TODO: Section 2 of TODO 2 (2 minutes for students, 1 minute demo)
 
-# ####################################################################################################
+####################################################################################################
 
 # TITLE  Section 3 (4 minutes)
 
@@ -92,7 +92,7 @@ print(string1, "I am python number", num3, ",hear me roar!!")
 
 # ####################################################################################################
 
-# TITLE  Section 4 (4 minutes)
+# TITLE  Section 4 (2 minutes)
 
 # Sometimes when you are dealing with variables, you are going to want to check the type of
 # the variable to make sure you are correctly using it. Python has a built in function for this!
@@ -107,6 +107,12 @@ print("num1:", type(num1))
 print("string1:", type(string1))
 print("bool1:", type(bool1))
 print("flt1:", type(flt1))
+
+# TODO: Section 4 of TODO 2 (2 minutes for students, 2 minute demo)
+
+####################################################################################################
+
+# TITLE  Section 5 (4 minutes)
 
 # Python also makes it pretty easy to convert types into other types. For example,
 # you can convert integers to strings by wrapping the variable in the string
@@ -130,4 +136,4 @@ print(floated_num)
 # Integer: integer()
 # Boolean: bool()
 
-# TODO: Section 4 of TODO 2
+# TODO: Section 5 of TODO 2 (5 minutes for students, 3 minute demo)
