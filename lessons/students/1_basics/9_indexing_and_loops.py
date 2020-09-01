9721cb6f7a32fdab0bb121af65e80fdc77dacbcc
palmerigarrett <palmerigarrett@gmail.com>
2020-09-01 18:50:11 -0400

    pre_lesson_9 modifications

--- lessons/students/1_basics/9_indexing_and_loops.py
+++ lessons/students/1_basics/9_indexing_and_loops.py
@@ -15,7 +15,7 @@ Indexing and range function for lists, dictionaries, strings and numbers
 
 ####################################################################################################
 
-# TITLE: Section 1: FOR loops
+# TITLE: Section 1- FOR loops
 # "for x in range(y)" is a loop that would iterate through every number from zero up until the
 # given number of "y". When there is only one number passed to the range() function, the "lower bound" 
 # of that range would default to zero. The "upper bound", which in the case below is 9, would NOT be
@@ -28,7 +28,7 @@ for num in range(9):
 
 ####################################################################################################
 
-# TITLE: Section 1.1: Loops with range 
+# TITLE: Section 1.1- Loops with range 
 # If you put in an initial argument, that number is included in the range and would be the
 # "lower bound". The second number would be the "upper bound", and therefore not included
 # in the loop.
@@ -36,11 +36,11 @@ print("range 3, 11 loop: ")
 for num in range(3, 11):
     print(num)
 
-# TODO: Section 1 of the TODO
+# TODO: Section 1 of TODO 9
 
 ####################################################################################################
 
-# TITLE: Section 2: String Indexing
+# TITLE: Section 2- String Indexing
 # If you want to access a part of a string, you can get a portion of the string with indexing.
 beatles = "You say goodbye, and I say hello!"
 
@@ -53,7 +53,7 @@ print("letters from the beatle song: " + beatles[3:11])
 
 ####################################################################################################
 
-# TITLE: Section 2.1: Lists and Loops
+# TITLE: Section 2.1- Lists and Loops
 
 # Index lists with for loops.
 grades = [72, 87, 99, 45, 70]
@@ -84,16 +84,9 @@ for num in grades:
 
 print("End of second for loop. \n")
 
-# Lets find out how many scores were in our list.
-# We do this by using the len() function with the "grades" list
-# to print us the length, or the number of items in the list.
-print("Number of students: " + str(len(grades)))
-
-# TODO: Section 2 of the TODO
-
 ####################################################################################################
 
-# TITLE: Section 3: Using a dictionary in a loop
+# TITLE: Section 2.2- Using a dictionary in a loop
 
 # Let's make reading grades easier by storing each grade as a value to the 
 # student's name as its key in the dictionary. When you use a for loop,
@@ -102,9 +95,10 @@ print("Number of students: " + str(len(grades)))
 
 grades = {"Chris": 65, "Deshaun": 77, "Mariah": 88, "Paula": 94}
 
-# Here is how you access values in a dictionary- you pass it the key!
-# The format for this is value = dict[key]
-print(f"Chris got a {grades["Chris"]}")
+# Here is how you access values in a dictionary and store it into a variable witht the format:
+# value = dict[key]
+chris = grades["Chris"]
+print(f"Chris got a {chris}")
 
 # When you use a for loop with a dictionary, the variable gets set to every key, NOT the value :)
 # It is important to understand that functions and loops use a colon and work within a 'scope', 
@@ -113,103 +107,103 @@ print(f"Chris got a {grades["Chris"]}")
 for student in grades:
     print(f"{student} got a {grades[student]} on their comp sci exam")
 
-# Lets find out how many students had scores!
-# We do this by using the len() function with the "grades" dictionary
-# to print us the length, or the number of key/value pairs in the dictionary
-print("Number of students: " + str(len(grades)))
+# HINT:
+# We can use bracket notation inside the for loop to make it easier to reference the given values.
+print("\n2nd for loop:\n")
+for student in grades:
+    grade = grades[student]
+    print(f"{student} got a {grade} on their comp sci exam")
+
+# TODO: Section 2 of TODO 9
 
 ####################################################################################################
-# TITLE: Section 3.1: While loops
 
+# TITLE: Section 3- While loops
 
-# The way that a for loop/while loop decides to continue running is if it evaluates a condition
-# as true. We will go over what is considered "True" for different types of iterable
-# data (i.e. lists, dicts, range) using loops.
-
-# This will continue to run until all the items have been removed with the .pop method
-# and your list is empty. An empty list equates to a False condition, and the loop will end.
+# The way that a while loop decides to continue running is if it evaluates a condition as true.
+# The following while loop will continue to run until all the items have been removed with the
+# .pop() method and your list is empty. This is because while there are items in the list, the
+# condition remains true. An empty list equates to a False condition, and the loop will end.
 grocery_list = ['Bread', 'Butter', 'Frozen Pizza', 'Mozz Sticks', 'Ice Cream']
 while grocery_list:
     purchased_item = grocery_list.pop()
     print(f'You purchased {purchased_item} and removed it from your list.')
     print('Your remaining items are', grocery_list)
 
-# TODO: Section 3 of the TODO
-
-####################################################################################################
-# TITLE: Section 4: While Loop incraments 
-
-# If you have a number that you are decrementing, the while loop will run until that number
-# is zero. A zero value equates to a False condition, and the loop will then end.
+# If you want to execute an action a certain number of times, you can use an iterator with a while
+# loop to decrement until that number is 0. A zero value will equate to a False condition, thus
+# ending the while loop.
 i = 10
 while i:
-    print(i)
+    print(f"i ={i}")
     i -= 1
 
-# TODO: Section 4 of the TODO
+# TODO: Section 3 of TODO 9
 
 ####################################################################################################
-# TITLE: Section 5: Tying it all together 
 
-# "continue" statement:
-# The continue statement rejects all the remaining statements in the current iteration of the loop
+# TITLE: Section 4- Continue Statements and Intro to Conditionals
+
+# The "continue" statement rejects all the remaining statements in the current iteration of the loop
 # and moves the control back to the top of the loop.
 
 # In this example, we provide a sneak peak at a conditional. The condition we are using is an "if"
 # condition. That just means "if the following condition is true, then do something within the scope
-# of this statement". While we also haven't really addressed scope, 
-# just know that everything following the if statement that is tabbed inward represents 
-# the scope of that if statement.
-# Example:
-if (str(5) == '5'):
+# of this statement". While we also haven't really addressed scope, just know that everything
+# following the if statement that is tabbed inward represents the scope of that if statement.
+# Below is an example to get you comfortable with how conditionals work.
+if (str(5) == '5'): # "if the integer '5' converted to a string is equal to the string '5"
     print('this is in the scope of the if statement')
     print('this would also be in the scope of the if statement')
 
 print('this would NO LONGER be in the scope of the if statement')
 
-# The continue statement can be used in both while and for loops.
-for letter in 'Python': # this will iterate through every letter in the word Python.
+# Here we will show examples of using a continue statement, as well as a conditional, within a for
+# loop and a while loop.
+for letter in 'Python': # This will iterate through every letter in the word Python.
    if (letter == 'h'):
       continue
    print(f"Current Letter :{letter}")
 
-var = 10
-while var > 0:
-    var -= 1 # this will subtract one from the "var" variable and set it equal to that new value
-    if(var == 5):
+i = 10
+while i > 0: # Here we are using a condtional (while i is greater than 0), to set custom
+               # True/False conditions for our while loop.
+    i -= 1 # This will subtract one from the "i" variable and set it equal to that new value.
+    if(i == 5):
         print("This iteration, since the if statement condition was true, we will trigger the",
-        " continue statement. That will skip the current iteration and not execute the print ",
+        "continue statement. That will skip the current iteration and not execute the print",
         "statement within the while loop. \n")
         continue
     print(f"Current variable value :{var}")
 
-# this would execute after the while loop is done running.
-print("Good bye!") # since this tabulation is set to the same spacing as the while loop,
-                   # this print statement won't run until the while loop is complete.
+# This will execute after the while loop is done running.
+print("Goodbye!") # This print statement won't run until the while loop is complete
+                  # since this tabulation is set to the same spacing as the while loop.
 
-# "break" statement:
+####################################################################################################
 
+# TITLE: Section 4.1- Break Statements
+
+# "break" statements are used with conditionals to stop a loop from running before its natural end.
+# Normally in a for loop or while loop, the natural end is when the base condition is False whether
+# it be iterating through the last item in a list or an iterator hitting 0 in a while loop. Adding a
+# break statement when a sub-condition is True though will end the for loop or while loop no matter
+# what iteration the program is on. Below is an example of this within a for loop.
 for letter in 'Python':
-    if letter == 'h': # this will, again, iterate through every letter in the word Python.
-        break
+    if letter == 'h': # Again, this will iterate through every letter in the word Python.
+        break # When the variable "letter" is eequal to "h", the loop will break.
     print(F"Current Letter :{letter}")
 
 var = 10
-# You can see that custom conditions can be placed in a while loop, too. 
-# We can check for the following conditions:
-# "x == y" (x is equal to y); "x != y" (x is NOT equal to y); 
-# "x > y" (x is greater than y); "x >= y" (x is greater than or equal to y);
-# "x < y"(x is less than y); "x <= y" (x is less than or equal to y); 
-# "x in y" (x exists in y- often y represents a list here);
-# "x not in y" (x does not exist in y); 
-# and we can combine conditions such as "x < y and x != z" (x is less than y AND x does not equal z)
-
-while var > 2: # this loop will only run if "var" is greater than 2.
-    var -= 1 # this will subtract one from the "var" variable and set it equal to that new value
+while var > 2: # This loop will only run if "var" is greater than 2.
+    var -= 1 # This will decrement the "var" variable by one (1).
     if (var == 5):
-        print("This iteration, since the if statement condition was true, will trigger the break statement. That end the current iteration and then end the for loop. \n")
+        print("This iteration, since the if statement condition was true, will trigger the break",
+              "statement. This will end the current iteration and subsequently the while loop.\n")
         break
-    print(f"Current variable value : {var}")
-print("Your while loop has ended. Good bye!") # since this tabulation is set to the same spacing as the while loop, this will run once the loop is complete
+    print(f"Current variable value: {var}")
+print("Your while loop has ended. Goodbye!") # Since this tabulation is set to the same
+                                             # spacing as the while loop, this will run once
+                                             # the loop is complete.
 
-# TODO: Section 5 of the TODO
+# TODO: Section 4 of TODO 9
