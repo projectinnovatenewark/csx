07c87b6107331c3cb43cc7a5a82c2cbae5e59095
palmerigarrett <palmerigarrett@gmail.com>
2021-01-10 20:47:08 -0500

    Lesson 2r and 3

--- lessons/students/1_basics/3_string_manipulation.py
+++ lessons/students/1_basics/3_string_manipulation.py
@@ -8,24 +8,20 @@ This is an introduction to string manipulation in Python
 # Lets set the user inputs equal to variables, then use them appropriately.
 first_name = input("What is your first name?: ")
 last_name = input("What is your last name?: ")
+age = int(input("What is your age?: ")) # FIXME: Teacher, use type conversion after getting an error.
 
-# Inputs automatically set a user input to a string- so what if we wanted to ask someone
-# for their age and have it set equal to an integer? We can wrap the entire input statement
-# in an int() function, and then the user's number input would be converted into a variable.
-# As you recall from last lesson, we can also convert values to strings with the str() function.
-age = int(input("What is your age?"))
-print("I am", str(age), "years old.")
+# As you can see below, inputs automatically set a user input to a string.
+print(f"The variable age has a type of {type(age)}.")
 
-# Since the values you input are set to variables, we can print them next to plan old strings.
-# Additionally, since we want a period at the end of the sentence, we will use a "+" sign so that
-# there isnt an extra space after the last_name variable (recall- spaces occur when you use commas).
-print("My first name is", first_name, "and my last name is", last_name + ".")
+# The line below will try to concatenate the string variable "age" and the integer "1". This will
+# result in an error because as we learned previously, strings and integers cannot be conatenated.
+print(f"My name is {first_name} {last_name} and next year I will be {age + 1} years old.")
 
-# Additionally, if you want a user to enter an input without having any words on the line of that
-# input, you can use an empty input function.
+# Additionally, if you want a user to enter an input on a blank line you can use an empty input
+# function.
 print("Enter the name of your favorite movie on the next line:")
 movie = input()
-print("Your favorite movie is", movie)
+print(f"Your favorite movie is {movie}.")
 
 # TODO: Section 1 of TODO 3 (2 minutes for students, 1 minute demo)
 ####################################################################################################
@@ -33,74 +29,53 @@ print("Your favorite movie is", movie)
 # TITLE: Section 2 - Modifying the Casing of Strings (3 minutes)
 # Next we are going to introduce the .title() function. This will capitalize the beginning of every
 # word in a string as well as make the rest of the characters lower case.
-
 thor2 = "tHOr: the DARK wOrlD".title()
 print(thor2)
 
 # There are also casing functions for upper and lower cases, which are used the same as title().
-# They are .upper() and .lower(). We will explore these below.
-
+# We will explore .upper() and .lower() below.
 mixed_string = "whAT Does THIS dO?"
 
-print(mixed_string.lower())
-print(mixed_string.upper())
+print(mixed_string.lower()) # This will change all characters to lower case.
+print(mixed_string.upper()) # This will change all characters to upper case.
 
 # New string tools! You can start a string with '\n' when printing it to print
 # on a new line, which would add a line above it. "\" is a special character, also called
 # the "escape" character. It is used in representing certain 'whitespace characters' or
 # adding space between characters. : "\t" is a tab and "\n" is a newline,
-
 print("\n" + mixed_string.lower())
-
 print("\t" + mixed_string.title())
 
 # TODO: Section 2 of TODO 3 (4 minutes for students, 1 minute demo)
-####################################################################################################
+##################################################################################################
 
-# TITLE: Section 3 - Formatting Variables in Strings (10 minutes)
+# TITLE: Section 3 - Formatting Variables in Strings (5 minutes)
 
 # TIP: This section can be of minor emphasis as it is not seen very often in our curriculum.
-# There are many methods of string formatting including the .format() function and using
-# percent signs "%" to include numbers. Here are brief examples of each:
+# There are many methods of formatting strings. One of which is the use of a percent sign "%" to
+# format the amount of decimal places in a float.
 integer_number = 3
 print("Here is the number as a float: %.2f" %integer_number)
 
-# TIP: This section can be of minor emphasis as it is not seen very often in our curriculum.
-# The cleaner way to do the above is strictly by using the format function as shown below.
-# You can also modify a number (i.e. 06 below) to add padding to the front of a number
-# which means that the first number will print with 6 digits (including the decimal point)
-# and add any additional digits as zeros in front of the number.
+# The cleaner way to do the above is by using the format function. The .format() function lets us
+# concatenate elements within a string through positional formating.
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+print("My name is {} {}.".format(first_name, last_name))
+
+# You can also use the .format() function to modify the number of decimal places just as the % does.
+# To do so, you use the format ":num_of_zeros.num_of_decimal_placesf"
 integer_number = 6
 float_number = 5.1234
 print("Here is first variable passed into format {:06.2f} and here is the second {:.2f}".format(integer_number, float_number))
 
-# IMPORTANT:
-# Here we will introduce the primary method with which we will be inserting variables directly
-# into strings, called "f shorthand". Let's set some variables and insert them in a string that has
-# a lowercase "f" right before the first quotation mark in the string.
-
-num1, num2, num3 = 3, 6, 9
-song_lyric = f"{num1}, {num2:.1f}, {num3:.2f} transit line." # You can also include formatting like
-                                                             # our previous examples with ":.2f"
-
-print(song_lyric)
-
-# When we explore lists and dictionaries in the future, you will see that items from lists and
-# key/value pairs from dictionaries can also be directly inserted into the f shorthand curly braces.
-# The first item in "students" is "Marcus", and we access the first item with the
-# convention "students[0]". The first item in a list is always zero, the second item is one, etc.
-students = ["Marcus", "Maria", "Sam"]
-
-attendance_output = f"The present students are {students[0]}, {students[1]}, and {students[2]}."
-print(attendance_output)
-
-# TODO: Section 3 of TODO 3 (5 minutes for students, 2 minute demo)
 ####################################################################################################
 
 # TITLE: Section 4 - String-related Functions (9 minutes)
-# The join function combines items in a list. In this case, it will create a large string.
-# The first input specifies how to join the items in the list.
-# In the example below, it will join each item with a comma.
+
+# The join function combines items in a list. In this case, it will create a large string. The first
+# input specifies how to join the items in the list. In the example below, it will join each item
+# with a comma.
 list_to_join = ["combine", "these", "words", "into", "one", "string"]
 print(f"Join the list_to_join {','.join(list_to_join)}")
 
@@ -113,34 +88,36 @@ print(f"As you can see, the original variable list_to_join has not changed {list
 new_var = "".join(list_to_join)
 print(f"This is the new var: {new_var}")
 
-# But that looks kind of silly without spaces! Let's join them with a space instead to see
-# the better outcome. We can reassign the new_var variable to have this new value.
+# But that looks kind of silly without spaces! Let's join them with a space instead to see a cleaner
+# outcome. We can reassign the new_var variable to have this new value.
 new_var = " ".join(list_to_join)
 print(f"This is the new_var's updated value: {new_var}")
 
-# Split seperates a string into items in a list. The split occurs in the character that you specify.
-# For example, in the split we put an empty space. This will split the string into items whenever
-# there is a space, so each word will represent an item in the list.
+# The split function seperates a string into items in a list. The split occurs in every instance of
+# the character that you specify. For example, in the split we put an empty space. This will split
+# the string into list items wherever there is a space.
 string_to_listify = "lets separate these words into separate items in a list"
-print(f"Split the string_to_listify {string_to_listify.split(' ')}") # FIXME: demo this with an "s", as well!
+print(f"Split the string_to_listify {string_to_listify.split(' ')}") # FIXME: Teacher, demo this
+                                                                     # FIXME: with an "s", as well!
 
-# The behavior for the join function also applies to split. It will NOT update the original
+# Split shares the same behavior as the join function. It will NOT update the original
 # variable. This will still be a string, even though we performed a split function on it.
 print(f"As you can see, the original variable string_to_listify has not changed {string_to_listify}")
 
-# If we wanted to capture the value of that split, we would have to set it equal to a new variable.
+# Just as before, if we wanted to capture the value of that split, we will have to set it equal to a
+# new variable.
 listified_string = string_to_listify.split(" ")
 print("this is listified_string's updated value: ", listified_string)
 
-# The replace function exchanges one input for another.
-# Lets make the following polite statement a little less formal:
+# The replace function exchanges one input for another. Lets make the following polite statement a
+# little less formal:
 print("Hello, how are you?".replace("Hello", "Sup"))
 
 # The startswith function returns a boolean value (True/False) on whether or not a
 # string begins with a certain set of characters. Lets check if someone is saying hello
 # in the following two statements:
 print("Hello, I am saying hi to someone".startswith("Hello"))
-print("Whats good?".startswith("Hello"))
+print("Whats up?".startswith("Hello"))
 
 # The endswith function returns a boolean value (True/False) on whether or not a string ends
 # with a certain set of characters. Lets check if these statements are questions:
