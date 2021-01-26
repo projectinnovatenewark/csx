835f89b655f74d2fdfce0d639e524524a6364a59
palmerigarrett <palmerigarrett@gmail.com>
2021-01-26 16:00:00 -0500

    added review lessons

---
+++ reviews/sem_1_review_exercises/exercise_1.py
@@ -0,0 +1,43 @@
+""" Pre-Reqs: vars_and_types, basic_math, imports """
+
+# FIXME: #1 Write import statement here
+
+# TODO: E-1
+
+# Set a variable to each data type we've learned using the following variable names: fruit,
+# day_born, velo, is_wet.
+
+# Your first variable should be a string and equal to your favorite fruit.
+# Your second variable should be an integer and equal to the day of the month you were born.
+# Your third variable should be a float and equal to the velocity of gravity: 9.8.
+# The last variable should be a boolean and set to True or False based on the following
+# question: Is water wet?
+
+# In their own print statements, print each variable on its own line.
+
+# TODO: E-2
+
+# Define a variable called first_name equal to your first name. Then print, "Hello my name
+# is [first_name]." using f shorthand.
+
+# Next, define another variable called last_name and print a statement similar to the one above,
+# but this time using both first_name and last_name.
+
+# Finally create a print statement that includes your first name, last name, and a variable 
+# from E-1. EX: "My name is Jon Smith and the velocity of gravity is 9.8 mph."
+
+# TODO: E-3
+
+# Write a python program to convert feet to inches and store the answer in a variable called inches.
+# HINT: There are 12 inches in 1 foot.
+
+num_feet = 4
+
+# TODO: E-4
+
+# The print statement below will throw you an error. Revise and uncomment the print statement so you
+# can run it without receiving an error. #HINT: Errors in Python are generally descriptive!
+
+three = '3'
+# print(f"3 Plus 10 is {three + 10}")
+
---
+++ reviews/sem_1_review_exercises/exercise_2.py
@@ -0,0 +1,43 @@
+""" 
+  Pre-Reqs:
+  vars_and_types,
+  basic_math,
+  imports,
+  functions_and_scope,
+  Sem 1 - Workshop_2: Pseudocode
+"""
+
+# FIXME: #1 Write import statement here
+
+
+# TODO: E-1
+
+# To begin this section, import "pi" *from* the "math" package in FIXME: #1.
+
+# Below is a function called "findArea1()". Inside it, write some code that will find the area of a
+# circle. The user should be able to input the value of "r" (the circle's radius). Your function
+# should lastly print the statement, "The area of a circle with a radius of [r] is [area]." using
+# f shorthand.
+ 
+# TIP:
+# The equation for finding the area of a circle is πr² = area.
+
+def findArea1():
+  pass # FIXME: Delete this. "pass" is used in python to ignore the function when nothing is in it.
+
+# TODO: E-2
+
+# Next create a new function called "findArea2()" that finds the area of a circle, but this time
+# takes the radius as a parameter instead of a user input. Then, if the area is larger than 10,
+# print, "This is a large circle." Otherwise print, "This circle isn't that impressive."
+
+# TODO: E-3
+
+# Lastly create a function called "findArea3()" to find the radius of a circle, but this time return
+# the value of its area. Again, the function should take "radius" as a paremeter. Then create a
+# seperate function called "printer()" that calls "findArea()" and prints its return value in the
+# statement, "The area of the new circle is [area]." The radius should again be passed as an
+# argument to the "findArea()" function.
+
+# HINT:
+# How can we store the value of an executed function?
