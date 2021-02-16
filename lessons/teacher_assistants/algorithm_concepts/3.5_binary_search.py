f2b17fac5516708b71a601e7cfe60bac15d6ace8
Christian <chrisadrianotav@gmail.com>
2021-02-15 21:01:58 -0500

    add in binary search, bst, and three primary sorting algos

--- lessons/teacher_assistants/algorithm_concepts/3.5_binary_search.py
+++ lessons/teacher_assistants/algorithm_concepts/3.5_binary_search.py
@@ -1,21 +1,56 @@
-# Binary search on a one dimensional list
+"""Two different implementations of Binary Search"""
 
-def binary_search(low, high, target):
-    if (low == high or low + high <= 0):
-        if (nums[low] == target):
-            return low
-        else:
-            return -1
-        
+# TITLE Iterative Binary Search
+
+def iterative_binary_search(arr, x):
+  low = 0
+  high = len(arr) - 1
+  mid = 0
+
+  while low <= high:
+    mid = (high + low) // 2
+
+    # If x is greater, ignore left half
+    if arr[mid] < x:
+      low = mid + 1
+
+    # If x is smaller, ignore right half
+    elif arr[mid] > x:
+      high = mid - 1
+
+    # means x is present at mid
     else:
-        mid_point = (low + high) / 2
-        mid_point = int(mid_point)
+      return mid
 
-        if (target == nums[mid_point]):
-            return mid_point
-        if (target < nums[mid_point]):
-            return binary_search(low, mid_point-1, target)
-        else:
-            return binary_search(mid_point+1, high, target)
-        
-return binary_search(0, len(nums)-1, target)
+  # If we reach here, then the element was not present
+  return -1
+
+listy = [1, 3, 4, 7, 9, 10, 15, 16, 20]
+print("Iterative Binary Search")
+print(iterative_binary_search(listy, 4))
+
+# TITLE Recursive Binary Search
+
+def recursive_binary_search(arr, x, left, right):
+
+  pivot = (left + right) // 2
+
+  if (right >= left):
+    # Base Case: pivot, or mid point, is equal to the target
+    if (arr[pivot] == x):
+      return x
+    
+    # pivot is less than the target, recurse for right subarray
+    elif (arr[pivot] < x):
+      return recursive_binary_search(arr, x, pivot+1, right)
+
+    # pivot is greater than the target, recurse for left subarray
+    else:
+      return recursive_binary_search(arr, x, left, pivot-1)
+
+  else:
+    return -1
+
+listy = [1, 3, 4, 7, 9, 10, 15, 16, 20]
+print("Recursive Binary Search")
+print(recursive_binary_search(listy, 4, 0, len(listy)-1))
--- lessons/teacher_assistants/search_sort_and_analysis/10_sorting_algorithms.py
+++ lessons/teacher_assistants/search_sort_and_analysis/10_sorting_algorithms.py
@@ -1,26 +1,79 @@
 """
-Introduce sorting algorithms including insertion sort, selection sort, and bubble sort.
+Introduce sorting algorithms including insertion sort, mergesort, and quicksort.
 """
 
-# FIXME: Explain insertion sort
+# Two less common sorting algorithms are bubble sort and selection sort.
+# Due to the infrequency with which they're used, we will leave them to
+# a future video and detail the methods and time/space complexities there.
+
+# TITLE: Insertion Sort
 def insertion_sort(some_list):
-    # for every element in our array
-    for index in range(1, len(some_list)):
-        current = some_list[index]
-        position = index
+  # for every element in our array, execute the following:
+  for index in range(1, len(some_list)):
+    current = some_list[index]
+    position = index
 
-        while position > 0 and some_list[position-1] > current:
-            print("Swapped {} for {}".format(some_list[position], some_list[position-1]))
-            some_list[position] = some_list[position-1]
-            print(some_list)
-            position -= 1
+    # if current num is less than previous num, swap them till current num is in sorted position
+    while position > 0 and some_list[position-1] > current:
+      print("Swapped {} for {}".format(some_list[position], some_list[position-1]))
+      some_list[position] = some_list[position-1]
+      print(some_list)
+      position -= 1
 
-        some_list[position] = current
+      some_list[position] = current
 
-    print("Sorting Completed.")
-    print(some_list)
+  return some_list
 
 arr = [12, 11, 2, 13, 5, 6, 19, 9, 17]
-insertion_sort(arr)
+print(f"Insertion sort result {insertion_sort(arr)}")
 
 ##################################################################################################
+
+# TITLE: Mergesort
+def merge(left, right):
+  both = []
+  left_idx, right_idx = 0, 0
+  while left_idx < len(left) and right_idx < len(right):
+    if (left[left_idx] < right[right_idx]):
+      both.append(left[left_idx])
+      left_idx += 1
+    else:
+      both.append(right[right_idx])
+      right_idx += 1
+  
+  if (left_idx == len(left)):
+    both.extend(right[right_idx:])
+  else:
+    both.extend(left[left_idx:])
+  return both
+
+def mergesort(listy):
+  if (len(listy) <= 1):
+    return listy
+  midpoint = len(listy) // 2
+  left, right = mergesort(listy[:midpoint]), mergesort(listy[midpoint:])
+  return merge(left, right)
+
+arr = [12, 11, 2, 13, 5, 6, 19, 9, 17]
+print(f"Mergesort result {mergesort(arr)}")
+
+##################################################################################################
+
+# TITLE: Quicksort
+def quicksort(listy):
+  if (len(listy) <= 1):
+    return listy
+
+  pivot = (len(listy)-1) // 2
+  pivot_num = listy.pop(pivot)
+  smaller, larger = [], []
+  for num in listy:
+    if (num < pivot_num):
+      smaller.append(num)
+    else:
+      larger.append(num)
+  
+  return quicksort(smaller) + [pivot_num] + quicksort(larger)
+
+arr = [12, 11, 2, 13, 5, 6, 19, 9, 17]
+print(f"Quicksort result {quicksort(arr)}")
