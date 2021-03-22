25512d58797154d95cabce0a90585359c7082d11
palmerigarrett <palmerigarrett@gmail.com>
2021-03-21 20:11:57 -0500

    lesson 14 and todo

--- lessons/students/2_functions_and_conditions/14_function_values/14_function_values.py
+++ lessons/students/2_functions_and_conditions/14_function_values/14_function_values.py
@@ -1,147 +1,123 @@
 """ 
 In this lesson, we will go over some more function work as well as using a 
-funcitons return value in other functions.
+functions return value in other functions.
 """
 
-# First, let's review a concept from lesson 6: pop(). If you recall, pop()
-# is used to remove items from a list (typically the last item) and return that value.
-# Here's a little refresher on how that works!
+# TITLE: Section 1 - Reviewing Return Values
 
-teams = ["NY Liberty", "Chicago Sky", "Atlanta Dream"]
-popped_team = teams.pop()
+# To begin, let's review a concept from lesson 6: the ".pop()" method. The ".pop()" method is a
+# built-in Python method that is used to remove elements from a list and return that element.
+# After returning the element, you can also store it in a variable to be used later.
+# TIP: Remember that the ".pop()" method will remove the last item in the list by default unless an
+# TIP: index is passed as an argument.
+
+teams = ["New York Liberty", "Chicago Sky", "Atlanta Dream"]
+popped_team = teams.pop() # This removes the last item in the list and stores it in "popped_team".
 print(popped_team)
 
-# This is possible because pop() is an in-built python function and returns the value of pop().
-# This practice is also possible by using 'return' in custom python functions which we will be
-# going over in this lesson. 
+# Similar to the example above, using a "return" statement in custom Python functions allows you to
+# access the value of the executed function. Below is a simple example to jog your memory from
+# Lesson 11:
 
-###################################################################################################
+def addTwo(num):
+  num += 2
+  return num # This is a "return" statement.
 
-# Let's walk through how to use functions and return values with one another. We will
-# write a program that specifies what type of pizza pie a user wants with separate functions
-# for each of the following. One function should ask a user what size they want and only
-# accept small, medium, or large. Another function should ask about toppings, but tell the
-# user they don't have pepperoni (if they ask for pepperoni). Our last function should confirm
-# the order with the user.
-
-def sizeInquiry():
-    """Ask a user what size pizza they want and return it if it is a valid response"""
-
-    sizeOfPie = input("What size pie do you want? (small, medium, or large): ")
-    acceptableInputs = ["small", "medium", "large"]
-    if (sizeOfPie not in acceptableInputs):
-        print("Try again and enter a valid pizza size.")
-        return False
-    else:
-        return sizeOfPie
-
-def toppingsInquiry():
-    """
-    Ask a user what toppings they want- make sure to let them know we dont 
-    have pepperoni if they ask!
-    """
-
-    toppings = input("What toppings would you like to have with your pizza?: (separate your toppings with commas and no spaces in between) ")
-    # Lets split the user's string into separate items in a list. We want to make sure 
-    # they didnt try to order pepperoni!
-    listOfToppings = toppings.split(",")
-
-    if ("pepperoni" in listOfToppings):
-        followUp = input("Sorry dog, we don't have pepperoni. Do you want to change your order? (y/n) ")
-        if (followUp == "y"):
-            return False
-        if (followUp == "n"):
-            listOfToppings.remove("pepperoni")
-            toppingsString = ", ".join(listOfToppings)
-            return toppingsString
-    else:
-        return toppings
-
-def formatOrder(s, t):
-    """Create a basic formatted string to output to our user"""
-
-    userMessage = f"Your order is for a {s} pizza with {t}"
-    return userMessage
-
-def main():
-    """Guide a user through ordering a pizza"""
-
-    # The return value of a function call can be set equal to a variable.
-    size = sizeInquiry()
-    if (size is False):
-        # If the input value was incorrect and returned False, then we want to return 
-        # a main function call to start over.
-        return main()
-
-    toppings = toppingsInquiry()
-    if (toppings is False):
-        # If the input value was incorrect and returned False, then we want 
-        # to return a main function call to start over.
-        return main()
-
-    message = formatOrder(size, toppings)
-    print(message)
-
-main()
+num_plus_two = addTwo(20) # Just as when using the ".pop()" method, the value of "addTwo()" will be
+                          # stored in the variable "num_plus_two".
+print(num_plus_two)
 
 ####################################################################################################
 
-# Let's develop a small program using multiple functions to create a game. A user will input how
-# many times they want to play the game. The game is to guess a number between one and ten.
-# If they guess the number correctly, the program should tell the user they won. If they guess
-# incorrectly, it should tell them they were wrong.
+# TITLE: Section 2 - Using Custom Functions Together
 
-# Python has some pre-defined packages you can import functions from. We will explore this more in-
-# depth later in the course. The "random" package has a function used to generate a random integer.
+# Creating one large function often does not produce the most readable code. Therefore, it is
+# considered best practice to break the function up into smaller, "helper" functions. Let's walk
+# through this concept using the control of the functions below.
+
+# TODO: Hey Teacher, walk through the control with the class starting at "Control 1" marked by the
+# TODO: "TODO" tag.
+
+def inputWidth():
+  # Control 3: Use the built in "input()" function to get the width from the user and return it.
+  width = float(input("Please enter the width of your rectangle (Only input a float or integer): "))
+  return width
+
+def inputLength():
+  # Control 5: Use the built in "input()" function to get the length from the user and return it.
+  length = float(input("Please enter the length of your rectangle (Only input a float or integer): "))
+  return length
+
+def findArea():
+  width = inputWidth() # Control 2: Call "inputWidth()" and store its value in the variable "width"
+  length = inputLength() # Control 4: Call "inputWidth()" and store its value in the variable
+                         # "length"
+
+  area = width * length # Control 6: Calculate the area using the return values stored in "width"
+                        # and "length"
+
+  return area # Return "area" to be used later in the program.
+
+# TODO: Start walking through the control on the line below.
+rectangle_area = findArea() # Control 1: Call "findArea()" and store its value in the variable
+                            # "rectangle_area".
+
+print(f"The area of your rectangle is {rectangle_area} feet.")
+
+###################################################################################################
+
+# TITLE: Section 3 - Functions and Loops
+
+# Let's develop a small program using multiple functions to create a game where a user guesses a
+# number between one and ten. The program should then let the user input how many time they want to
+# play the game and tell them whether they have won or lost after each round.
+
+# The game will use the "random" package to produce a random number that the user is trying to
+# guess, as seen below.
 
 from random import randint # Remember import statements generally belong at the top of a file.
 
 def inquireUserGames():
-    """ask a user how many times they want to play"""
+    """Ask a user how many times they want to play."""
 
-    timesToPlay = input("How many times would you like to play the 'Guess the Number' game?: ")
-    numTimesToPlay = int(timesToPlay)
-    userStartedGameString = f"You've signed on to play {timesToPlay} games!"
-    # Let's return TWO variables to display some cool Python functionality. We can now
-    # set two variables equal to the value of calling this function.
-    return numTimesToPlay, userStartedGameString
+    num_games = int(input("How many times would you like to play the 'Guess the Number' game?: "))
+    num_games_string = f"You've signed on to play {num_games} games."
+
+    return num_games, num_games_string # Just as you can define multiple variables at once, you can
+                                       # also return multiple values from a single function.
 
 def playTheGame():
-    """ask user for a number between 1 and 10, generate a random number between 1 and 10, then see
-    if those numbers match. If they do, congratulate the user! If not, tell 'em they guess wrong."""
+    """Ask the user for a number between 1 and 10, generate a random number between 1 and 10, and
+     check if those numbers match. If they do, congratulate the user! If not, tell them they were
+     incorrect."""
 
-    # Generate a random number between 1 and 10.
-    randomNumber = randint(1, 10)
-    # Get an input from the user.
-    promptUserForNumber = input("Guess a number between 1 and 10: ")
-    # Convert their input into an integer so you can check for equality between
-    # the input and the random number generated.
-    userNumber = int(promptUserForNumber)
+    random_number = randint(1, 10) # Generate a random number between 1 and 10.
+    
+    user_guess = int(input("Guess a number between 1 and 10: "))
 
     # Check if the random number and user number equal each other.
-    if (randomNumber == userNumber):
-        print("You guessed correctly! Number", userNumber, "was the correct guess.", "\n")
+    if (random_number == user_guess):
+        print(f"You guessed correctly. Number {user_guess} is correct.\n")
     else:
-        print("Wrong Answer!!! Number", randomNumber, "doesnt equal", userNumber, "\n")
+        print(f"Your guess is incorrect! Number {random_number} doesnt equal {user_guess}.\n")
 
 def initializeGame():
     """Lets initialize our game and call the inquireUserGames function."""
 
-    # The first variable will equal the first returned value, and the second variable
-    # will equal the second returned value.
-    userGameCount, userWelcomeString = inquireUserGames()
-    print("\n", userWelcomeString, "\n")
+    # Recall that in the function "inquireUserGames()" will return 2 values. We use a comma to
+    # separate the returned values when storing them into variables.
+    game_count, welcome_string = inquireUserGames()
+    print(f"\n{welcome_string}\n")
 
-    # userGameCount will equal the user's input from their desired number of games to play.
-    # userWelcomeString will equal the output "You've signed on to play {userGameCount} games!"
-    # This while loop will continue to run until userGameCount equals zero.
-    while userGameCount:
+    # The variable "game_count" will equal the user's input for their desired number of games to
+    # play. The variable "welcome_string" will equal the output "You've signed on to play
+    # {game_count} games!"
+    while game_count: # This while loop will continue to run until "game_count" equals zero.
         playTheGame()
-        # Every time we run the playTheGame function we want to decrement the userGameCount
+        # Every time we run the "playTheGame()" function we want to decrement the game_count
         # by one until it hits zero, at which point the while loop will stop running.
-        userGameCount -= 1
+        game_count -= 1
 
-# Lets call the initializeGame function to get things going!
 initializeGame()
 
-# TODO: Complete TODO 13
+# TODO: Complete TODO 14 (10 min for students, 5 min for demo)
