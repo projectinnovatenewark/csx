6d25048b7406789568ab4f1a579220aea5906daa
Christian <chrisadrianotav@gmail.com>
2022-12-03 11:36:23 -0500

    minor grammar + file/dir name changes

--- year_1/semester_1/lessons/students/2_python/11_conditions/11_conditionstodo.py
+++ year_1/semester_1/lessons/students/2_python/11_conditions/11_conditionstodo.py
@@ -14,7 +14,7 @@ num3 = -45
 # Write a condition that prints "num1 is greater than num3" if num1 is greater than num3.
 # If that condition isn't true, then print "num1 is NOT greater than num3".
 
-# Write a conditional that prints f"{num2} + {num3} is equal to 55" if num2 plus num3 equals 55.
+# Write a condition that prints f"{num2} + {num3} is equal to 55" if num2 plus num3 equals 55.
 # HINT: There is more than one way to do this. You can use the expression in the conditional or
 # HINT: store the expression in a variable and base your condition off of that.
 
@@ -22,7 +22,7 @@ num3 = -45
 
 # TODO: Section 2:
 
-# Write a condition that tests if both bool1 AND bool2 are true. If this is true, print, "Everything
+# Write a condition that tests if both bool1 AND bool2 are true. If this is True, print, "Everything
 # is true". If this condition isn't true, test if either bool1 OR bool2 is True. If so, print, 
 # "One of these is true"
 # HINT: What conditional can be used to test a specific condition only if the above is false?
