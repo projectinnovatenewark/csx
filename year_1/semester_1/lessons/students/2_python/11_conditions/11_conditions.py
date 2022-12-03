53b050ae14de87d8e3cff75498dfc80c5acc0cf1
Christian <chrisadrianotav@gmail.com>
2022-12-03 13:03:12 -0500

    slight improvements to conditions lesson

--- year_1/semester_1/lessons/students/2_python/11_conditions/11_conditions.py
+++ year_1/semester_1/lessons/students/2_python/11_conditions/11_conditions.py
@@ -1,5 +1,5 @@
 """
-if, elif, else and iteratable operations
+Understanding Conditions
 """
 
 # TITLE: Section 1 - If Statements (5 minutes)
@@ -10,42 +10,55 @@ if, elif, else and iteratable operations
 bool1 = True
 bool2 = False
 
-if (bool2 == True): # TODO: Teacher, change the if statement from "bool1" to "bool2".
-  print("It's true! It's true!")
+if bool2: # TODO: Teacher, change the if statement from "bool1" to "bool2".
+    print("This is true.")
 
-# In plain english, the example above states that IF bool1 is true, the print statement
-# "It's true! It's true!" will be printed.
+if True: # TODO: Teacher, change the if statement from True to False.
+    print("This is true.")
 
-# You might have noticed the "==". Using a "==" is Python for "equal to". We can't use a single "="
-# because Python would think you are setting a variable. Below are some of the many ways you can
-# test conditions (formatted as "symbol" : "plain english"):
-# == : equal to
-# != : is not equal to
-# > : greater than
-# >= : greater than or equal to
-# < : less than
-# <= : less than or equal to
+# In plain english, the example above states that if bool1 is true, the print statement
+# "This is true." will be printed.
 
+# You might have noticed the "==". Using a "==" is Python for "equal to". Recall that a
+# single "=" is to assign a value to a variable. Below are some of ways to write conditions
+# in the format of "code" (meaning):
+# == (equal to)
+# != (is not equal to)
+# > (greater than)
+# >= (greater than or equal to)
+# < (less than)
+# <= (less than or equal to)
+
+# When you write one of these conditions, it will evaluate to True or False, which then
+# looks a lot like the condition "if True" that we reviewed above. Let's demonstrate a
+# couple of examples.
+
+print("Condition 1: ", "a"=="b")
+print("Condition 2: ", "a"=="a")
+print("Condition 3: ", "a"!="b")
+print("Condition 4: ", "a"!="a")
+print("Condition 5: ", 1>2)
+print("Condition 6: ", 1<2)
+
+# Now let's plug in one of these conditions into an if statement. Check the strings and
+# condition closely, what do you think the output will be?
 pin = "Project Innovate Newark"
-
-if (pin != "Project Initiate Newark"): # Testing if the value of "pin" is not equal to the string,
-                                       # "Project Initiate Newark"
-    print("This isn't what PIN stands for!") # TODO: Will this statement be printed?
+if pin != "Project Initiate Newark":
+    print("This isn't what PIN stands for!")
 
 ####################################################################################################
 
-# TITLE: Section 1.1 - Else Statements (6 minutes)
+# TITLE: Section 1.1 - Else Statement (6 minutes)
 # An else statement will be executed when all other conditions haven't been met.
 # An else statements always comes after an if statement and cannot be used as a 
 # standalone condition.
 
 num = 2 # TODO: Teacher, try changing this number up to satisfy the different conditions.
 
-if (num > 20): 
-  print("It's true! It's true!")
-else: # Here we use an else statement to say, "if the above condition is not true, then execute
-      # the following code"
-  print("The variable is not greater than 20!")
+if num > 20: 
+    print("This number is greater than 20.")
+else: # As for the else clause, it means "if the above condition isn't true, then execute this code."
+    print("This number is not greater than 20.")
 
 # You can see in the above that num is not greater than 20, so the statement "The variable is not
 # greater than 20!" is printed. There isn't a condition in an else statement because it only runs if
@@ -55,10 +68,10 @@ else: # Here we use an else statement to say, "if the above condition is not tru
 # be printed?
 pin = "Project Innovate Newark"
 
-if (pin == "Project Initiate Newark"):
-  print("This is what PIN stands for!")
+if pin == "Project Initiate Newark":
+    print("This is what PIN stands for!")
 else:
-  print("This isn't what PIN stands for!")
+    print("This isn't what PIN stands for!")
 
 ####################################################################################################
 
@@ -70,10 +83,10 @@ else:
 
 num1 = 10
 
-if (num1 < 10):
-  print("The number is less than 10.")
-elif (num1 > 5):
-  print("The number is greater than 5.")
+if num1 < 10:
+    print("The number is less than 10.")
+elif num1 > 5:
+    print("The number is greater than 5.")
 
 # The output here is "the number is greater than 5". Python first evaluate the if statement. Since
 # num1 is not less than 10, the elif is executed.
@@ -83,10 +96,10 @@ elif (num1 > 5):
 
 num2 = 6
 
-if (num2 < 10):
-  print("The number is less than 10.")
-elif (num2 > 5):
-  print("The number is greater than 5.")
+if num2 < 10:
+    print("The number is less than 10.")
+elif num2 > 5:
+    print("The number is greater than 5.")
 
 # Only the if statement was executed. That's where the "else" of the elif comes in. Remeber that an
 # elif statement will only run if the above conditions are not met.
@@ -101,14 +114,14 @@ elif (num2 > 5):
 
 num = 3 # TODO: Teacher, try changing this number up to satisfy the different conditions.
 
-if (num > 0):
-  print(f"{num} is a positive number.")
-if (num > 2): # Any number of if statements will be evaluated upon running the code
-  print(f"{num} is greater than 2.")
-elif (num < 0): # The elif statement will only be triggered if none of the "if" statements are true.
-  print(f"{num} is a negative number.")
+if num > 0:
+    print(f"{num} is a positive number.")
+if num > 2: # Any number of if statements will be evaluated upon running the code
+    print(f"{num} is greater than 2.")
+elif num < 0: # The elif statement will only be triggered if none of the "if" statements are true.
+    print(f"{num} is a negative number.")
 else: # The else statement will only run if none of the conditions above it are true.
-  print(f"{num} is a zero value.")
+    print(f"{num} is a zero value.")
 
 # TODO: Section 1 of TODO 11 (5 minutes for students, 2 min for demo)
 
@@ -127,26 +140,26 @@ else: # The else statement will only run if none of the conditions above it are 
 # conditions returns false though, the entire if statement will return false and the block of code
 # will not run.
 
-if (True and True): # A True condition AND a True condition will return true
-  print("True and True is true")
+if True and True: # A True condition AND a True condition will return true
+    print("True and True is true")
 
-if (True and False): # A False condition AND a True condition will return false
-  print("True and False is true")
+if True and False: # A False condition AND a True condition will return false
+    print("True and False is true")
 
-if (False and False): # A False condition AND a False condition will return false
-  print("False and False is true")
+if False and False: # A False condition AND a False condition will return false
+    print("False and False is true")
 
 # Using "or" is different in that only one condition needs to be me for the if statement to 
 # return True. So, "If one item is true OR another item is true, then return True." The "or" will
 # cause the if statement to return true as long as one condition is satisfied.
 
-if (True or True): # A True condition OR a True condition will return true
-  print("True or True is true")
+if True or True: # A True condition OR a True condition will return true
+    print("True or True is true")
 
-if (True or False): # A False condition OR a True condition will return true
-  print("True or False is true")
+if True or False: # A False condition OR a True condition will return true
+    print("True or False is true")
 
-if (False or False): # A False condition OR a False condition will return false
-  print("False or False is true")
+if False or False: # A False condition OR a False condition will return false
+    print("False or False is true")
 
 # TODO: Section 2 of TODO 11 (4 minutes for students, 2 min for demo)
