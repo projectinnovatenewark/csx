9e06cffec20160069c33ffa8e5fd457321b329a8
Tasnim Tabassum <tasnimsimi99@gmail.com>
2023-02-02 13:02:32 -0500

    added a period and _between two words

--- Students/Semester2/lessons/archive/19_debugging/19_debugging_and_logging.py
+++ Students/Semester2/lessons/archive/19_debugging/19_debugging_and_logging.py
@@ -2,16 +2,16 @@
 Fix this broken code and teach students what debugging looks like
 """
 
-# FIXME: this code is broken and has explanations of what is wrong with it. also, change the
-# FIXME: variable of num = 15 so that a variable of num takes in a user input. num_analyzer 
-# FIXME: should test every number from the user input all the way down to on.
+# FIXME: this code is broken and has explanations of what is wrong with it.
+# FIXME: Change the variable of num = 15 so that a variable of num takes in a user input. 
+# FIXME: num_analyzer should test every number from the user input all the way down to on.
 
 def isPrime(n):
-    """isPrime should take a number as an argument and return true if it is prime or false if it is not"""
+    """isPrime should take a number as an argument and return true if it is prime number or false if it is not"""
     truth_val = True
     # FIXME: this problem was supposed to range through numbers between 2 and n
     for n in (2,n): #missing range function; avoid double-assigning variable in for loop and range function; re-name for loop variable
-        if num % n == 0: #argument in if statement should start with arbitrary number THEN for range variable 
+        if num % n == 0: #argument in if statement should start with arbitrary number. THEN for range variable 
             truth_val = False
             print("Condition is satisfied") #this is an exmple of logging to check if the function is at least being tested.
         break # when would this break statement occur?
@@ -22,8 +22,8 @@ isPrime(13)
 ####################################################################################################
 
 def num_analyzer(): #argument needed in function definition
-    """numAnalyzer should accept an argument and return if it is even or false AND if it is prime
-    it shold test from every number """
+    """num_analyzer should accept an argument and return if it is an even number or false AND if it is prime
+    it should test from every number """
     # FIXME: this problem is supposed to decrement by one to test
     # FIXME: all numbers between argument passed and zero
     while (num > 0):
