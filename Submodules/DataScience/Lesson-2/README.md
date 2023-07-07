94f81998ba93e308f56fe3c7f8f42d32d3128328
Vineel1015 <vin.nad1015@gmail.com>
2023-07-06 20:38:22 -0400

    move files around and define pandas lesson in the Lesson 2 folder

---
+++ Submodules/DataScience/Lesson-2/README.md
@@ -0,0 +1,5 @@
+# Data with Pandas
+
+To start, you should fork off of this [repl](https://replit.com/@Christiantav/data-science-pandas?v=1). It will include the python packages you need (pandas and others) to run the code in this lesson.
+
+<Insert description of the lesson>
--- Submodules/DataScience/Lesson-3/config.py
+++ Submodules/DataScience/Lesson-3/config.py
@@ -1,17 +1,8 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-
-# The second argument for .get() method is what the variable will be equal to if the first arg isn't defined in the environment (.env file).
-
-user = os.environ.get('POSTGRES_USER', 'pg user not found')
-password = os.environ.get('POSTGRES_PASSWORD', 'pg password not found')
-host = os.environ.get('POSTGRES_HOST', 'pg host not found')
-database = os.environ.get('POSTGRES_DB', 'pg database not found')
-port = os.environ.get('POSTGRES_PORT', 'pg port not found')
+user = '<YOUR_POSTGRES_USER>'
+password = '<YOUR_POSTGRES_PASSWORD>'
+host = '<YOUR_POSTGRES_HOST>'
+database = '<YOUR_POSTGRES_DB>'
+port = '<YOUR_POSTGRES_PORT>'
  
 def get_connection():
     return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
-
-print(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
